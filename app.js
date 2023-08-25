214acd5daa934f157096bca4b7f36c3192987c04
Sahil khatri <Sahilkehsav1997@gmail.com>
2023-08-25 18:51:24 +0530

    add routes and controllers

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const port = 5000
+
+const tasks = require('./routes/tasks')
+
+app.use('/api/v1/tasks', tasks)
+
+app.get('/', (req, res) => {
+  res.send('Task manager api')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`)
+})
